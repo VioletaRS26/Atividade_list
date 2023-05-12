7102b0555e821609ef97a88b9aae479adf07377b
W. Fernando <wellysonf@gmail.com>
2023-05-11 21:05:32 -0300

    atualização dos states

---
+++ src/screens/components/Participant/index.jsx
@@ -0,0 +1,17 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import { styles } from './style';
+export function Participant({name, remove}) {
+    return(
+    <View style={styles.containerP}>
+        <Text style={styles.name}>
+            {name}
+        </Text>
+
+        <TouchableOpacity style={styles.buttonRm} onPress={remove}>
+            <Text style={styles.buttonText}>
+                -
+            </Text>
+        </TouchableOpacity>
+    </View>
+    );
+}
